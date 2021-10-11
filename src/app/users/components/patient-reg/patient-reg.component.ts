a6e9c591ef2496e2a456a66074b61e9291a870f3
Yogendra Yadav <yogendrakumar.yadav@citiustech.com>
2021-10-11 19:20:10 +0530

    PAtient redirect

--- src/app/users/components/patient-reg/patient-reg.component.ts
+++ src/app/users/components/patient-reg/patient-reg.component.ts
@@ -84,17 +84,6 @@ export class PatientRegComponent implements OnInit {
     this.formSubmitAttempt = true;
     console.log(this.form);
 
-    // const newPatient = new Patient(
-    //   this.form.value.title,
-    //   this.form.value.firstName,
-    //   this.form.value.lastName,
-    //   this.form.value.email,
-    //   this.form.value.birthDate,
-    //   this.form.value.contactNo,
-    //   this.form.value.username,
-    //   this.form.value.password
-    // );
-
     if (this.form.valid) {
       const newPatient = new User();
       newPatient.title = this.form.value.title;
@@ -109,12 +98,10 @@ export class PatientRegComponent implements OnInit {
       this.userService.addPatient(newPatient);
 
       // When no user in session redirect after signup to login page
+      // Redirect to Login Page when Sign up by new Patient
       if (!this.user) {
         this.router.navigate(['/']);
       }
-
-      // Redirect to Login Page when Sign up by new Patient
-      // this.router.navigate(['auth']);
 
       // Redirect To Dashboard after registration
     }
