17297a5114dadbdcc470a2bcf3bfcc11a26597de
Ketan pimpalshende <ketan.pimpalshende@citiustech.com>
2021-10-26 18:09:01 +0530

    changes done

--- src/app/inbox/inbox.service.ts
+++ src/app/inbox/inbox.service.ts
@@ -1,77 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Inbox } from '../inbox.model';
 import { InboxData } from '../model/inbox.model';
 import { User } from '../model/user.model';
 import { AuthService } from '../services/auth.service';
 import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class InboxService {
-  listOfData: Inbox[] = [
-    new Inbox(
-      1,
-      'Covid-19',
-      'dhoni1',
-      '2021-9-11',
-      '4:30',
-      `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-      true
-    ),
-    new Inbox(
-      2,
-      'Covid-19',
-      'dhoni2',
-      '2021-9-11',
-      '4:30',
-      `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-      true
-    ),
-    new Inbox(
-      3,
-      'Covid-19',
-      'dhoni3',
-      '2021-9-11',
-      '4:30',
-      `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-      false
-    ),
-    new Inbox(
-      4,
-      'Covid-19',
-      'dhoni4',
-      '2021-9-11',
-      '4:30',
-      `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-      false
-    ),
-    new Inbox(
-      5,
-      'Covid-19',
-      'dhoni5',
-      '2021-9-11',
-      '4:30',
-      `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-      true
-    ),
-    new Inbox(
-      6,
-      'Covid-19',
-      'dhoni6',
-      '2021-9-11',
-      '4:30',
-     `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-     false
-    ),
-    new Inbox(
-      7,
-      'Covid-19',
-      'dhoni7',
-      '2021-9-11',
-      '4:30',
-     `Coronavirus disease (COVID-19) is an infectious disease caused by the SARS-CoV-2 virus.`,
-     false
-    ),
-  ];
   constructor(private http: HttpClient,private authService: AuthService){}
   staffNameList: User[] = [];
   patientNameList: User[] = [];
@@ -123,17 +57,4 @@ export class InboxService {
   getAllPaientData(): Observable<User[]> {
     return this.http.get<User[]>(`${this.HOST_URL}/appointments/patients`);
   } 
-
-getAllappointments(){
-  return this.listOfData.filter(ob=>ob.declined===false);
-}
-getDeclined(){
-  return this.listOfData.filter(ob=>ob.declined===true);
-}
-  //delete method
-  deleteRecord(args:Inbox){
-    const pos = this.listOfData.indexOf(args);
-    //this.listOfData.splice(pos, 1);
-    this.listOfData[pos].declined=true;
-  }
 }
